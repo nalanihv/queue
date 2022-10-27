f0d9e6576661de22c9b3b04d62df13b5f24819f2
Nalanihv <nalanihevi@gmail.com>
2022-10-27 18:00:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/queue/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.queue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private  Queue queue;
+    private EditText etUsuario;
+    private Button btGuardar;
+    private Button btQuitar;
+    private Button btMostrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsuario.findViewById(R.id.etUsuario);
+        btGuardar.findViewById(R.id.btGuardar);
+        btMostrar.findViewById(R.id.btMostrar);
+        btQuitar.findViewById(R.id.btQuitar);
+
+        queue= new Queue();
+        queue.add(2);
+        queue.printl();
+        queue.remove();
+
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input=etUsuario.getText().toString().trim();
+
+                if( input.equals("") )
+                    Toast.makeText(getApplicationContext(),"ingresa un dato", Toast.LENGTH_SHORT).show();
+                else{
+                    int aux=Integer.parseInt(input);
+                    queue.add(aux);//se supone que aqui agrega
+                }
+
+            }
+        });
+        btQuitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/queue/Queue.java
@@ -0,0 +1,62 @@
+package com.example.queue;
+
+import android.util.Log;
+
+public class Queue {
+    private QueueNode first;
+    private QueueNode last;
+
+    public Queue() {//por que la primera vez estan vacios -Se inicializa
+        first=null;
+        last=null;
+        //first=last=null;
+    }
+
+    public QueueNode getFirst() {
+        return first;}
+    public QueueNode getLast() {
+        return last;}
+
+
+
+    public void add(int data){
+        QueueNode node=new QueueNode(data);//agrega valores
+        if (node==null){
+            Log.i("log","Queue overflow");
+        }else {
+            if(first==null){
+                first=node;//el primero siempre va a tener el mismo valor agregando
+            }else {
+                last.setNext(node);//el dato que entra se enlaza con el set el enlace para que apunte al ultimo de ese momento , hasta
+                //que se actualiza en la siguente linea. Al ultimo ponerle el que sigue despues de el.
+            }
+            last=node;//el ultimo que llegue es el ultimo
+        }
+    }
+
+    public void printl(){
+        String result="";
+        QueueNode aux=first;
+        while (aux!=null){ // aux==last.getNext; sign.
+            result+=aux+" -> ";
+            aux=aux.getNext();
+        }
+        Log.i("log",""+result);
+    }
+
+
+    public int remove(){
+
+        if (first==null){
+            return -1;//no hay elementos
+        }else {
+            QueueNode aux=first;
+            first=first.getNext();//el primero va hacer ahora el enlace que tiene
+            if (first==null)
+                last = null;
+
+
+            return aux.getData();
+
+        }}
+}
